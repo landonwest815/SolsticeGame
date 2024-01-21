b1cd567a543b2357f6a5160301d50ae599d91d4d
Landon West <landonwest815@gmail.com>
2024-01-21 10:31:16 -0700

    Tweaked Deer Movement/Animation

--- deer.gd
+++ deer.gd
@@ -10,17 +10,25 @@ var direction = Vector2.ZERO
 var normalized_direction = Vector2.ZERO
 var last_direction = Vector2.ZERO
 
+var cooldown_timer = Timer.new()
+
 # called when the node is added to the scene (initializer)
 func _ready():
 	
 	# set the animation
 	animated_sprite = $AnimatedSprite2D
+	
+	# Configure and add the cooldown timer
+	cooldown_timer.set_wait_time(0.1)  # Set the timer to 1 second for cooldown
+	cooldown_timer.set_one_shot(true)
+	add_child(cooldown_timer)
 
 func _physics_process(delta):
-	if is_fleeing:
-		position -= (player.position - position)/speed
-		direction = player.position - position
-		normalized_direction = direction.normalized()
+	if is_fleeing or not cooldown_timer.is_stopped():
+		if player != null:
+			direction = player.position - position
+			normalized_direction = direction.normalized()
+			position -= normalized_direction * speed * delta
 	else:
 		normalized_direction = Vector2.ZERO
 		
@@ -45,9 +53,10 @@ func _physics_process(delta):
 func _on_detection_area_body_entered(body):
 	player = body
 	is_fleeing = true
+	cooldown_timer.stop()
 
 func _on_detection_area_body_exited(body):
 	player = null
 	is_fleeing = false
-	animated_sprite.play("idle_right")
+	cooldown_timer.start()
 
